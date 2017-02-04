2a498323da1b6f1a1c1967b79f9e038dcbcc7430
Shun Kashiwa <shun.kashiwa@live.jp>
2017-02-04 16:27:48 +0900

    install and setup @ionic/storage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { IonicApp, IonicModule } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 
 import { MyApp } from './app.component';
 import { HomePage } from './../pages/home/home';
@@ -20,6 +21,9 @@ import { SendModal } from './../pages/details/sendModal';
         IonicModule.forRoot(MyApp)
     ],
     bootstrap: [IonicApp],
+    providers:[
+        Storage
+    ],
     entryComponents: [
         MyApp,
         HomePage,
